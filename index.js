501d026c7a9cbdd7e36f7bcdbc6fb27fe5231299
Vasil Vanchuk <v.vanchuk@tut.by>
2016-01-06 22:24:23 +0300

    Read file line-by-line

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-var Octokat = require('octokat');
+const Octokat = require('octokat');
+const LineByLineReader = require('line-by-line');
+
+const lr = new LineByLineReader('users.db');
+
+lr.on('error', function (err) {
+  console.log('[ERROR]', err);
+});
+
+lr.on('line', function (line) {
+  console.log(line);
+  
+});
+
+lr.on('end', function () {
+	console.log('[DONE]');
+});
